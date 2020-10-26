2981acc0244f52e05cecbed56c5ef176dbca04be
James Xu <54513773+fbjxu@users.noreply.github.com>
2020-10-26 15:32:26 -0700

    basic api for autocomplete

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+//services
 import { ComponentLayoutServiceService } from 'src/app/services/component-layout-service.service';
+import { DataServiceService } from 'src/app/services/data-service.service';
+
+import { autoCompleteEntry } from 'src/app/models/autoCompleteEntry';
 
 
 @Component({
@@ -10,11 +14,19 @@ import { ComponentLayoutServiceService } from 'src/app/services/component-layout
 })
 export class SearchComponent implements OnInit {
   title:string;
-  objectOptions =[
-    { name: "Angular"},
-    { name: "React"}
-  ]
-  constructor(private router: Router, public componentLayoutService: ComponentLayoutServiceService) { }
+  // objectOptions =[
+  //   { name: "Angular"},
+  //   { name: "React"}
+  // ]
+  objectOptions:autoCompleteEntry[];
+  showAutoLoading:boolean = true;
+  constructor(
+    private dataService:DataServiceService,
+    private router: Router, public componentLayoutService: ComponentLayoutServiceService) { 
+      this.dataService.getAutoComplete(this.title).subscribe((data:autoCompleteEntry[])=> {
+        this.objectOptions = data;
+      })
+  }
 
   ngOnInit(): void {
   }
