69373a37a09bff9059af7e9b251f7ef7de67dfa3
James Xu <54513773+fbjxu@users.noreply.github.com>
2020-10-27 09:09:47 -0700

    added autocomplete function

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -7,8 +7,8 @@ import { DataServiceService } from 'src/app/services/data-service.service';
 import { autoCompleteEntry } from 'src/app/models/autoCompleteEntry';
 //filter for autocomplete
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import { interval, Observable } from 'rxjs';
+import {debounceTime, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -20,25 +20,28 @@ export class SearchComponent implements OnInit {
   title:string;
   objectPreOptions:autoCompleteEntry[];
   objectOptions:string[];
-  showAutoLoading:boolean = true;
+  showAutoLoading:boolean = false;
   filteredOptions: Observable<string[]>;
 
   //methods
   constructor(
     private dataService:DataServiceService,
     private router: Router, public componentLayoutService: ComponentLayoutServiceService) { 
-      this.dataService.getAutoComplete("apple").subscribe((data:autoCompleteEntry[])=> {
-        this.objectPreOptions = data;
-        this.objectOptions = this.objectPreOptions.map(entry=>entry.ticker+" | "+entry.name);
-      });
   }
 
   ngOnInit(): void {
-    this.filteredOptions = this.myControl.valueChanges
-      .pipe(
-        startWith(''),
-        map(value => this._filter(value))
-      );
+    const result =  this.myControl.valueChanges.pipe(
+      tap(ev => this.showAutoLoading = true),
+      debounceTime(400)).
+    subscribe(value=>
+      { 
+        this.lookupAutoCompleteEntries(value);
+      });
+    // this.myControl.valueChanges.subscribe(value=>this.lookupAutoCompleteEntries(value));
+      // .pipe(
+      //   startWith(''),
+      //   map(value => this._filter(value))
+      // );
   }
 
   onSubmit() {    
@@ -47,14 +50,22 @@ export class SearchComponent implements OnInit {
     this.router.navigate(["details/",ticker])
   }
 
+  lookupAutoCompleteEntries(input: string) {
+    this.dataService.getAutoComplete(input).subscribe((data:autoCompleteEntry[])=> {
+      this.objectPreOptions = data;
+      this.objectOptions = this.objectPreOptions.map(entry=>entry.ticker+" | "+entry.name);
+      this.showAutoLoading = false;
+    });
+  }
+  
   displayFn(subject) {
     return subject? subject.ticker : undefined;
   }
 
   myControl = new FormControl();
 
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-    return this.objectOptions.filter(option => option.toLowerCase().includes(filterValue));
-  }
+  // private _filter(value: string): string[] {
+  //   const filterValue = value.toLowerCase();
+  //   return this.objectOptions.filter(option => option.toLowerCase().includes(filterValue));
+  // }
 }
