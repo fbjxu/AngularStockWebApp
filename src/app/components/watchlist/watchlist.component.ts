a903c0c8b16ae87cf74fad68b654a16e4c0f165a
James Xu <54513773+fbjxu@users.noreply.github.com>
2020-10-27 17:53:45 -0700

    attemp to add to watchlist

--- src/app/components/watchlist/watchlist.component.ts
+++ src/app/components/watchlist/watchlist.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ComponentLayoutServiceService } from 'src/app/services/component-layout-service.service';
+import { WatchlistmanagerService } from '../../services/watchlistmanager.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-watchlist',
@@ -7,13 +9,17 @@ import { ComponentLayoutServiceService } from 'src/app/services/component-layout
   styleUrls: ['./watchlist.component.css']
 })
 export class WatchlistComponent implements OnInit {
-
-  constructor(public componentLayoutService:ComponentLayoutServiceService) { 
+  currentList: string[];
+  currentObserve: Observable<string[]>;
+  
+  constructor(
+    private watchlistmanager:WatchlistmanagerService,
+    public componentLayoutService:ComponentLayoutServiceService) { 
     this.componentLayoutService.makeInvisible();
   }
 
   ngOnInit(): void {
-
+    this.currentList = this.watchlistmanager._watchlist;
   }
 
 }
